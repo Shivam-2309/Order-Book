6a849d69f2e0f01a72e6f44e8c0e396d5b390813
Shivam-2309 <116469216+Shivam-2309@users.noreply.github.com>
2025-07-20 16:00:14 +0530

    feat: Basic Structure Ready

--- OrderBook.cpp
+++ OrderBook.cpp
@@ -105,6 +105,65 @@ private:
 using OrderPointer = std :: shared_ptr<Order>;
 using OrderPointers = std :: list<OrderPointer>;
 
+class OrderModify 
+{
+public:
+    OrderModify(OrderId orderId, Side side, Price price, Quantity quantity)
+    : 
+    orderId_ { orderId }
+    , price_ { price }
+    , side_ { side }
+    , quantity_ { quantity }
+    {}
+
+    OrderId GetOrderId() const { return orderId_; }
+    Price GetPrice() const { return price_; }
+    Side GetSide() const { return side_; }
+    Quantity GetQuantity() const { return quantity_; }
+
+    // This helps to create a new object from an order modify object and convert it 
+    // and the new object that is made is having the new properties only
+    OrderPointer ToOrderPointer(OrderType type) const 
+    {
+        // This make_shared creates a smart pointer which manages memory of the order object
+        return std :: make_shared<Order>(type, GetOrderId(), GetSide(), GetPrice(), GetQuantity());
+        // What are smart pointers ?
+        // These are he objects which manages memory of other objects and delte them when not in use
+        // Better alternativr that using raw pointers
+        // Less debugging and Less chance of other dangling pointers
+    }
+
+private:
+    OrderId orderId_;
+    Price price_;
+    Side side_;
+    Quantity quantity_;
+};
+
+struct TradeInfo 
+{
+    OrderId orderId_;
+    Price price_;
+    Quantity quantity_;
+};
+
+class Trade 
+{
+public:     
+    Trade(const TradeInfo& bidTrade, const TradeInfo& askTrade)
+    :
+    bidTrade_ { bidTrade }
+    , askTrade_ { askTrade }
+    { }
+
+    const TradeInfo& GetBidTrade() const { return bidTrade_; }
+    const TradeInfo& GetAskTrade() const { return askTrade_; }
+
+private:
+    TradeInfo bidTrade_;
+    TradeInfo askTrade_;
+};
+
 int main() {
     std :: cout << "CHECK" << std :: endl;
     return 0;
